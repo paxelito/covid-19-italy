10a06c9a6bc16768c9328c97076d2fcef575bba1
Alessandro Filisetti <a.filisetti@explora-biotech.com>
2020-03-22 01:10:16 +0100

    notebook class based

---
+++ src/config.py
@@ -0,0 +1,21 @@
+import os
+from datetime import date, timedelta
+
+COVID_DATA_FOLDER = os.path.abspath(os.path.join('/Users/afilisetti/Documents/git-projects/COVID-19/'))
+CITIES_DATA_JSON_URI = os.path.abspath(os.path.join(COVID_DATA_FOLDER, "dati-json/dpc-covid19-ita-province.json"))
+REGIONS_DATA_JSON_URI = os.path.abspath(os.path.join(COVID_DATA_FOLDER, "dati-json/dpc-covid19-ita-regioni.json"))
+ITALY_MAP = fp = os.path.abspath(os.path.join(COVID_DATA_FOLDER, "aree/shp/dpc-covid19-ita-aree.shp"))
+
+AVG_GROWTH_RATE_WINDOW = 7
+
+NW_REGIONS = ["Lombardia", "Piemonte", "Valle d'Aosta", "Liguria"]
+NE_REGIONS = ["Emilia Romagna", "Veneto", "Friuli Venezia Giulia", "P.A. Bolzano", "P.A. Trento"]
+C_REGIONS = ["Lazio", "Toscana", "Umbria", "Marche"]
+S_REGIONS = ["Campania", "Abruzzo", "Molise", "Campania", "Puglia", "Basilicata"]
+ISLANDS = ["Sicilia", "Sardegna"]
+
+CITIES_LOMBARDIA = ['Bergamo', 'Brescia', 'Milano', 'Lodi', 'Monza e della Brianza', 'Sondrio', 'Varese', 'Lecco', 'Pavia', 'Cremona', 'Como', 'Mantova']
+CITIES_EMILIA = ['Piacenza', "Reggio nell'Emilia", 'Ravenna', 'Ferrara', 'Modena', 'Forlì-Cesena', 'Bologna', 'Parma', 'Rimini']
+CITIES_VENETO = ['Padova', 'Rovigo', 'Treviso', 'Verona', 'Belluno', 'Vicenza', 'Venezia']
+CITIES_CAMPANIA = ['Napoli', 'Benevento', 'Avellino', 'Salerno', 'Napoli', 'Caserta']
+
---
+++ src/dataclass/coviddata.py
@@ -0,0 +1,152 @@
+from datetime import date, timedelta
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import geopandas as gpd
+import numpy as np
+from src.config import CITIES_DATA_JSON_URI, REGIONS_DATA_JSON_URI, ITALY_MAP, AVG_GROWTH_RATE_WINDOW
+
+
+class ItalianCovidData:
+
+    def __init__(self, avg_growth_rate_window=AVG_GROWTH_RATE_WINDOW):
+        self.cities_data_json = pd.read_json(CITIES_DATA_JSON_URI)
+        self.cities_data_json["data"] = pd.to_datetime(self.cities_data_json["data"]).dt.strftime('%Y-%m-%d')
+        self.regions_data_json = pd.read_json(REGIONS_DATA_JSON_URI)
+        self.map_df = gpd.read_file(ITALY_MAP)
+
+        self.regions_data_json["data"] = pd.to_datetime(self.regions_data_json["data"]).dt.strftime('%Y-%m-%d')
+        self.regions_data_json["ratio_positivi"] = self.regions_data_json["nuovi_attualmente_positivi"] / self.regions_data_json["tamponi"]
+        self.regions_data_json["mortality"] = self.regions_data_json["deceduti"] / self.regions_data_json["totale_casi"]
+
+        self.avg_growth_rate_window = avg_growth_rate_window
+
+        self.today = date.today()
+        self.yesterday = date.today() - timedelta(days=1)
+
+    def data_summary(self):
+        print("\n --- REGIONS DATASET --- \n")
+        print(self.regions_data_json.info())
+        print("\n --- CITIES DATASET --- \n")
+        print(self.cities_data_json.info())
+
+    def show_map_cases(self, today=True):
+        filter_time = self.today.strftime('%Y-%m-%d') if today else self.yesterday.strftime('%Y-%m-%d')
+        today_data_json = self.cities_data_json.loc[(self.cities_data_json['data'] == filter_time) & (self.cities_data_json['sigla_provincia'] != "")]
+        ax = self.map_df.plot(figsize=(10, 10), alpha=0.4, edgecolor='k')
+        today_data_json.plot(kind="scatter",
+                             x="long",
+                             y="lat",
+                             alpha=0.5,
+                             s=today_data_json["totale_casi"] / 5,
+                             label="Totale Casi", figsize=(10, 7),
+                             c=today_data_json["totale_casi"],
+                             cmap=plt.get_cmap("jet"),
+                             colorbar=True,
+                             ax=ax)
+
+    def plot_region(self, region):
+        plt.figure(figsize=(20, 10))
+        plt.subplot(1, 2, 1)
+        ax = sns.lineplot(x="data",
+                          y="totale_casi",
+                          hue="sigla_provincia",
+                          linestyle='dotted',
+                          marker="o",
+                          data=self.cities_data_json.query(f"denominazione_regione == '{region}'"),
+                          )
+        plt.subplot(1, 2, 2)
+        ax.set_ylabel("Totale Cases")
+        bx = sns.lineplot(x="data",
+                          y="totale_casi",
+                          hue="sigla_provincia",
+                          linestyle='dotted',
+                          marker="o",
+                          data=self.cities_data_json.query(f"denominazione_regione == '{region}'"),
+                          )
+        bx.set_yscale('log')
+        bx.set_ylabel("log(Total Cases)")
+        plt.draw()
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
+        bx.set_xticklabels(bx.get_xticklabels(), rotation=90)
+
+    def plot_region_indicators(self, regions_area):
+        self._plot_regions(self.cities_data_json, regions_area, 'totale_casi')
+        vars_of_interest = ['totale_casi', 'deceduti', 'terapia_intensiva', 'tamponi', 'ratio_positivi', 'mortality']
+        for var_of_interest in vars_of_interest:
+            self._plot_regions(data=self.regions_data_json,
+                               data_filter=regions_area,
+                               y=var_of_interest)
+
+    @staticmethod
+    def _plot_regions(data, data_filter, y='totale_casi'):
+        plt.figure(figsize=(20, 10))
+        plt.subplot(1, 2, 1)
+        ax = sns.lineplot(x="data",
+                          y=y,
+                          hue="denominazione_regione",
+                          data=data.query(f"denominazione_regione in {data_filter}"),
+                          linestyle='dotted',
+                          marker="o"
+                          )
+        plt.subplot(1, 2, 2)
+        bx = sns.lineplot(x="data",
+                          y=y,
+                          hue="denominazione_regione",
+                          markers=True,
+                          dashes=True,
+                          data=data.query(f"denominazione_regione in {data_filter}"),
+                          linestyle='dotted',
+                          marker="o"
+                          )
+        bx.set_yscale("log")
+        bx.set_ylabel(f"log({y})")
+        plt.draw()
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
+        bx.set_xticklabels(bx.get_xticklabels(), rotation=90)
+
+    def growth_rates(self, areas, regions=True, area_target='denominazione_provincia', indicator='totale_casi'):
+        data = self.regions_data_json if regions else self.cities_data_json
+        growth_rates = dict()
+        for area in areas:
+            data_area = data.loc[data[area_target] == area]
+            growth_rate = list()
+            growth_rates[area] = dict()
+            for idx, i in enumerate(data_area[indicator]):
+                if idx > 0:
+                    if list(data_area[indicator])[idx - 1] > 1:
+                        temp_gr = i / list(data_area[indicator])[idx - 1]
+                        growth_rate.append((temp_gr, list(data_area['data'])[idx]))
+
+            growth_rates[area]['growth_rate'] = growth_rate
+            growth_rate_n, growth_rate_date = zip(*growth_rates[area]['growth_rate'])
+
+            avg_gr = list()
+            for idx, g in enumerate(growth_rate_n[0:len(growth_rate_n) - self.avg_growth_rate_window + 1]):
+                avg_gr.append(np.mean(growth_rate_n[idx:idx + self.avg_growth_rate_window]))
+
+            growth_rates[area]['avg_growth_rate'] = avg_gr
+
+        plt.figure(figsize=(20, 10))
+        plt.subplot(1, 2, 1)
+        for area in areas:
+            growth_rate_n, growth_rate_date = zip(*growth_rates[area]['growth_rate'])
+            plt.plot(
+                growth_rate_date[:],
+                growth_rate_n[:],
+                linestyle='solid',
+                marker="o"
+            )
+            plt.xlabel("Date (data starts from case 0)")
+            plt.ylabel("Growth Rate")
+            plt.title(f"Daily Growth Rate ({area})")
+        plt.legend(areas)
+        plt.xticks(rotation=90)
+        plt.subplot(1, 2, 2)
+        for area in areas:
+            plt.plot(growth_rates[area]['avg_growth_rate'][:], linestyle='solid', marker="o")
+            plt.xlabel(f"Average growth Factor (Floating window: {self.avg_growth_rate_window} days) from case 0")
+            plt.ylabel("AVG Growth Factor")
+            plt.title(f"AVG Daily Growth Factor ({area})")
+        plt.legend(areas)
+        plt.draw()
